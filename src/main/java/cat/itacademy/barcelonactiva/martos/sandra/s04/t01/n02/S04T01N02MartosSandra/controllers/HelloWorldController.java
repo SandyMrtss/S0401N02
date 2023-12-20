0a734b92dff7b29ee3d2244654cb50c71310183d
SandyMrtss <bebesubi2@gmail.com>
2023-12-20 11:40:34 +0100

    Initial commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s04/t01/n02/S04T01N02MartosSandra/controllers/HelloWorldController.java
@@ -0,0 +1,21 @@
+package cat.itacademy.barcelonactiva.martos.sandra.s04.t01.n02.S04T01N02MartosSandra.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public static String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom){
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
+    @GetMapping(value = {"/HelloWorld2/{nom}", "/HelloWorld2"})
+    public static String saluda2(@PathVariable(required = false) String nom){
+        if(nom != null){
+            return "Hola, " + nom + ". Estàs executant un projecte Maven";
+        }
+        else{
+            return "Hola, estàs executant un projecte Maven";
+        }
+    }
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/martos/sandra/s04/t01/n02/S04T01N02MartosSandra/S04T01N02MartosSandraApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.martos.sandra.s04.t01.n02.S04T01N02MartosSandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N02MartosSandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
