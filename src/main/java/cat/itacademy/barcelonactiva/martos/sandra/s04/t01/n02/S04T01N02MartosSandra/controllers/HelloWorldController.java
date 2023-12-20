94d9fdd353efc8195f1039e43bd98a8e3ebad0c0
SandyMrtss <bebesubi2@gmail.com>
2023-12-20 12:04:25 +0100

    change message

--- src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s04/t01/n02/S04T01N02MartosSandra/controllers/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s04/t01/n02/S04T01N02MartosSandra/controllers/HelloWorldController.java
@@ -7,12 +7,12 @@ public class HelloWorldController {
 
     @GetMapping("/HelloWorld")
     public static String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom){
-        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+        return "Hola, " + nom + ". Estàs executant un projecte Gradle";
     }
     @GetMapping(value = {"/HelloWorld2/{nom}", "/HelloWorld2"})
     public static String saluda2(@PathVariable(required = false) String nom){
         if(nom != null){
-            return "Hola, " + nom + ". Estàs executant un projecte Maven";
+            return "Hola, " + nom + ". Estàs executant un projecte Gradle";
         }
         else{
             return "Hola, estàs executant un projecte Maven";
